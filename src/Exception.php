f6fc5815d01b708c8a89874675e6b4cc697f0688
iio <hannes.forsgard@gmail.com>
2014-04-14 10:45:48 +0200

    PSR-4 and license

---
+++ src/Exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+namespace iio\libmergepdf;
+
+/**
+ * @author Hannes Forsgård <hannes.forsgard@fripost.org>
+ */
+class Exception extends \Exception
+{
+}
--- src/Pages.php
+++ src/Pages.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * This file is part of the libmergepdf package
- *
- * Copyright (c) 2012-13 Hannes Forsgård
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
  */
 
 namespace iio\libmergepdf;
