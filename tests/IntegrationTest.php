2a9d6cc9a15f3cf240c7a0b2fdbd4d0ef8ef05bc
Hannes Forsgård <hannes.forsgard@fripost.org>
2017-10-31 20:31:45 +0100

    Make integration test more solid

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -9,10 +9,15 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $target = __DIR__ . '/AA.pdf';
         @unlink($target);
 
-        $m = new Merger();
-        $m->addFile(__DIR__ . '/A.pdf');
-        $m->addFile(__DIR__ . '/A.pdf', new Pages('1'));
-        file_put_contents($target, $m->merge());
+        $merger = new Merger;
+        $merger->addFile(__DIR__ . '/A.pdf');
+        $merger->addFile(__DIR__ . '/A.pdf', new Pages('1'));
+        file_put_contents($target, $merger->merge());
+
+        $merger = new Merger;
+        $merger->addFile($target);
+        $merger->addFile($target, new Pages('1'));
+        file_put_contents($target, $merger->merge());
 
         $this->assertTrue(file_exists($target), "$target should be created");
     }
